d40b6e3a09acadffed81924b776df685c614857c
wangzhiyong69 <wangzhiyong69@jd.com>
2021-12-30 20:15:28 +0800

    增加生成国密双证书方法

---
+++ utils-sm-tls/src/main/java/utils/certs/CertsHelper.java
@@ -0,0 +1,279 @@
+package utils.certs;
+
+import com.google.common.base.Strings;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
+import org.bouncycastle.jce.X509Principal;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+import java.util.Map;
+import java.util.Vector;
+import java.util.function.BiConsumer;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public final class CertsHelper {
+
+    private CertsHelper() {
+    }
+
+    public static X500Name buildCertDN(String country,
+                                       String organization,
+                                       String organizationalUnit,
+                                       String commonName,
+                                       String province,
+                                       String locality,
+                                       String email) {
+        X500NameBuilder builder = new X500NameBuilder(BCStyle.INSTANCE);
+        addRDN(builder, BCStyle.C, country);
+        addRDN(builder, BCStyle.O, organization);
+        addRDN(builder, BCStyle.OU, organizationalUnit);
+        addRDN(builder, BCStyle.CN, commonName);
+        addRDN(builder, BCStyle.ST, province);
+        addRDN(builder, BCStyle.L, locality);
+        addRDN(builder, BCStyle.EmailAddress, email);
+        return builder.build();
+    }
+
+
+    public static X500Name buildCertDN(String country,
+                                       String organization,
+                                       String organizationalUnit,
+                                       String commonName) {
+        return buildCertDN(country, organization, organizationalUnit, commonName, null, null, null);
+    }
+
+
+    public static PKCS10CertificationRequest createCSRRequest(X500Name subject, PublicKey pubKey, PrivateKey priKey) throws OperatorCreationException {
+        SMPublicKey sm2SubPub = new SMPublicKey(pubKey.getAlgorithm(), (BCECPublicKey) pubKey);
+        PKCS10CertificationRequestBuilder csrBuilder = new JcaPKCS10CertificationRequestBuilder(subject, sm2SubPub);
+        ContentSigner signerBuilder = new JcaContentSignerBuilder("SM3withSM2")
+                .setProvider(BouncyCastleProvider.PROVIDER_NAME).build(priKey);
+        return csrBuilder.build(signerBuilder);
+    }
+
+
+    public static void buildSMCaTestCerts(File smHomeDir,
+                                          int nodes,
+                                          int gws,
+                                          int users,
+                                          long expire,
+                                          String password,
+                                          Map<ASN1ObjectIdentifier, String> subjectDNMap) throws Exception {
+
+        KeyPair rootKeyPair = SM2Util.generateKeyPair();
+        X500Name rootCaDN = CertsHelper.buildCertDN(
+                subjectDNMap.get(BCStyle.C),
+                subjectDNMap.get(BCStyle.O),
+                "root-ca",
+                "Root CA for test",
+                subjectDNMap.get(BCStyle.ST),
+                subjectDNMap.get(BCStyle.L),
+                null
+        );
+
+        KeyPair middleKeyPair = SM2Util.generateKeyPair();
+        X500Name middleCaDN = CertsHelper.buildCertDN(
+                subjectDNMap.get(BCStyle.C),
+                subjectDNMap.get(BCStyle.O),
+                "middle-ca",
+                "Middle CA for test",
+                subjectDNMap.get(BCStyle.ST),
+                subjectDNMap.get(BCStyle.L),
+                null
+        );
+
+        X509Certificate rootCACert = SmCertMarker.createRootCACert(rootKeyPair, rootCaDN, expire);
+        X509Certificate middleCACert = SmCertMarker.createMiddleCACert(rootKeyPair, rootCaDN, middleCaDN, middleKeyPair, expire);
+
+        File rootCaPerm = new File(smHomeDir, "sm2.rca.pem");
+        Files.write(rootCaPerm.toPath(), certToPem(rootCACert), StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+        File middleCaPerm = new File(smHomeDir, "sm2.oca.pem");
+        Files.write(middleCaPerm.toPath(), certToPem(middleCACert), StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+        KeyStore trustKeyStore = KeyStore.getInstance("JKS");
+        trustKeyStore.load(null, password.toCharArray());
+        trustKeyStore.setCertificateEntry("rca", rootCACert);
+        trustKeyStore.setCertificateEntry("oca", middleCACert);
+
+        BiConsumer<String, X509Certificate> certificateConsumer = (alias, cert) -> {
+            try {
+                trustKeyStore.setCertificateEntry(alias, cert);
+            } catch (KeyStoreException e) {
+                //ignore
+            }
+        };
+
+        for (int i = 0; i < nodes; i++) {
+            CertsHelper.buildNodeSMCerts(rootCACert, middleCACert, middleKeyPair, "node" + i, "node", password, smHomeDir, certificateConsumer);
+        }
+
+        for (int i = 0; i < gws; i++) {
+            CertsHelper.buildNodeSMCerts(rootCACert, middleCACert, middleKeyPair, "gw" + i, "gw", password, smHomeDir, certificateConsumer);
+        }
+
+        for (int i = 0; i < users; i++) {
+            CertsHelper.buildNodeSMCerts(rootCACert, middleCACert, middleKeyPair, "user" + i, "user", password, smHomeDir, certificateConsumer);
+        }
+
+        trustKeyStore.store(new FileOutputStream(new File(smHomeDir, "client.jks")), password.toCharArray());
+
+    }
+
+    public static void buildNodeSMCerts(X509Certificate rootCa,
+                                        X509Certificate middleCa,
+                                        KeyPair middleCaKeyPair,
+                                        String nodeCommonName,
+                                        String nodeOUName,
+                                        String password,
+                                        File outputPathDir,
+                                        BiConsumer<String, X509Certificate> certificateConsumer
+    ) throws Exception {
+
+        KeyPair sigKeyPair = SM2Util.generateKeyPair();
+        KeyPair encKeyPair = SM2Util.generateKeyPair();
+
+        X509Principal middleCaX509Principal = (X509Principal) middleCa.getSubjectDN();
+        String country = getValue(middleCaX509Principal, BCStyle.C);
+        String org = getValue(middleCaX509Principal, BCStyle.O);
+        String unit = getValue(middleCaX509Principal, BCStyle.OU);
+        String cn = getValue(middleCaX509Principal, BCStyle.CN);
+        String province = getValue(middleCaX509Principal, BCStyle.ST);
+        String locality = getValue(middleCaX509Principal, BCStyle.L);
+        String email = getValue(middleCaX509Principal, BCStyle.EmailAddress);
+
+        X500Name middleCaDN = CertsHelper.buildCertDN(country, org, unit, cn, province, locality, email);
+        X500Name signDN = CertsHelper.buildCertDN(country, org, nodeOUName, nodeCommonName, province, locality, email);
+        X500Name encDN = CertsHelper.buildCertDN(country, org, nodeOUName, nodeCommonName, province, locality, email);
+
+        Date expireDate = rootCa.getNotAfter();
+        long expire = expireDate.getTime() - System.currentTimeMillis();
+
+        X509Certificate signCert = SmCertMarker.createEntitySignCert(middleCaKeyPair, middleCaDN, signDN, sigKeyPair, expire);
+        X509Certificate encCert = SmCertMarker.createEntityEncCert(middleCaKeyPair, middleCaDN, encDN, encKeyPair, expire);
+
+        File zipFile = new File(outputPathDir, "sm2." + nodeCommonName + ".zip");
+
+        KeyStore signKeyStore = KeyStore.getInstance("pkcs12");
+        signKeyStore.load(null, null);
+        signKeyStore.setKeyEntry("sign", sigKeyPair.getPrivate(), password.toCharArray(), new Certificate[]{middleCa});
+
+
+        KeyStore encKeyStore = KeyStore.getInstance("pkcs12");
+        encKeyStore.load(null, null);
+        encKeyStore.setKeyEntry("enc", encKeyPair.getPrivate(), password.toCharArray(), new Certificate[]{middleCa});
+
+        KeyStore bothStore = KeyStore.getInstance("pkcs12");
+        bothStore.load(null, null);
+        bothStore.setCertificateEntry("RCA", rootCa);
+        bothStore.setCertificateEntry("OCA", middleCa);
+        bothStore.setCertificateEntry("enc", encCert);
+        bothStore.setCertificateEntry("sig", signCert);
+        bothStore.setKeyEntry("enc", encKeyPair.getPrivate(), password.toCharArray(), new Certificate[]{encCert, middleCa, rootCa});
+        bothStore.setKeyEntry("sig", sigKeyPair.getPrivate(), password.toCharArray(), new Certificate[]{signCert, middleCa, rootCa});
+
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)))) {
+
+            zipOutputStream.putNextEntry(new ZipEntry("password.txt"));
+            zipOutputStream.write(password.getBytes());
+
+            zipOutputStream.putNextEntry(new ZipEntry("sm2.rca.pem"));
+            zipOutputStream.write(certToPem(rootCa));
+
+            zipOutputStream.putNextEntry(new ZipEntry("sm2.oca.pem"));
+            zipOutputStream.write(certToPem(middleCa));
+
+            //sign
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.sign.key.pem", nodeCommonName)));
+            zipOutputStream.write(keyToPem(sigKeyPair.getPrivate()));
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.sign.cert.pem", nodeCommonName)));
+            zipOutputStream.write(certToPem(signCert));
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.sign.cert.cer", nodeCommonName)));
+            zipOutputStream.write(signCert.getEncoded());
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.sign.key", nodeCommonName)));
+            zipOutputStream.write(sigKeyPair.getPrivate().getEncoded());
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.sign.pfx", nodeCommonName)));
+            signKeyStore.store(zipOutputStream, password.toCharArray());
+
+            //enc
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.enc.key.pem", nodeCommonName)));
+            zipOutputStream.write(keyToPem(encKeyPair.getPrivate()));
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.enc.cert.pem", nodeCommonName)));
+            zipOutputStream.write(certToPem(encCert));
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.enc.cert.cer", nodeCommonName)));
+            zipOutputStream.write(encCert.getEncoded());
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.enc.key", nodeCommonName)));
+            zipOutputStream.write(encKeyPair.getPrivate().getEncoded());
+
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.enc.pfx", nodeCommonName)));
+            encKeyStore.store(zipOutputStream, password.toCharArray());
+
+            //both
+            zipOutputStream.putNextEntry(new ZipEntry(String.format("sm2.%s.both.pfx", nodeCommonName)));
+            bothStore.store(zipOutputStream, password.toCharArray());
+        }
+
+        if (certificateConsumer != null) {
+            certificateConsumer.accept(nodeCommonName + "-enc", encCert);
+            certificateConsumer.accept(nodeCommonName + "-sig", signCert);
+        }
+
+    }
+
+    private static byte[] certToPem(X509Certificate x509Certificate) throws IOException {
+        StringWriter sw = new StringWriter();
+        JcaPEMWriter pemWriter = new JcaPEMWriter(sw);
+        pemWriter.writeObject(x509Certificate);
+        pemWriter.flush();
+        return sw.toString().getBytes();
+    }
+
+    private static byte[] keyToPem(PrivateKey privateKey) throws IOException {
+        StringWriter sw = new StringWriter();
+        JcaPEMWriter pemWriter = new JcaPEMWriter(sw);
+        pemWriter.writeObject(privateKey);
+        pemWriter.flush();
+        return sw.toString().getBytes();
+    }
+
+    private static String getValue(X509Principal principal, ASN1ObjectIdentifier identifier) {
+        Vector vector = principal.getValues(identifier);
+        if (vector == null || vector.isEmpty()) {
+            return "";
+        }
+        return String.valueOf(vector.get(0));
+    }
+
+    private static void addRDN(X500NameBuilder builder, ASN1ObjectIdentifier identifier, String value) {
+        if (!Strings.isNullOrEmpty(value)) {
+            builder.addRDN(identifier, value);
+        }
+    }
+
+}
--- utils-sm-tls/src/test/java/utils/TestKeyStore.java
+++ utils-sm-tls/src/test/java/utils/TestKeyStore.java
@@ -1,12 +1,19 @@
 package utils;
 
-import org.bouncycastle.math.ec.custom.gm.SM2P256V1Curve;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 import org.junit.Test;
+import utils.certs.CertsHelper;
+import utils.certs.SM2Util;
+import utils.certs.SmCertMarker;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -15,18 +22,162 @@ import java.util.UUID;
 public class TestKeyStore {
 
     @Test
-    public void test() throws Exception{
+    public void testBuildCertZipFile() throws Exception {
+
+        long expire = 365 * 24 * 60 * 60 * 1000;
+
+        KeyPair rootKeyPair = SM2Util.generateKeyPair();
+        KeyPair middleKeyPair = SM2Util.generateKeyPair();
+        X500Name rootCaDN = CertsHelper.buildCertDN("CN", "jd", "jd", "rootCA");
+        X500Name middleCaDN = CertsHelper.buildCertDN("CN", "jd", "jd", "middleCA");
+
+        X509Certificate rootCACert = SmCertMarker.createRootCACert(rootKeyPair, rootCaDN, expire);
+        X509Certificate middleCACert = SmCertMarker.createMiddleCACert(rootKeyPair, rootCaDN, middleCaDN, middleKeyPair, expire);
+
+        CertsHelper.buildNodeSMCerts(rootCACert, middleCACert, middleKeyPair, "node0-test", "node", "12345678", new File("D:\\block160\\cert\\"), null);
+
+
+    }
+
+
+    @Test
+    public void testCert() throws Exception {
+
+        long expire = 365 * 24 * 60 * 60 * 1000;
+
+        KeyPair rootKeyPair = SM2Util.generateKeyPair();
+        KeyPair middleKeyPair = SM2Util.generateKeyPair();
+        KeyPair node0SigKeyPair = SM2Util.generateKeyPair();
+        KeyPair node0EncKeyPair = SM2Util.generateKeyPair();
+        X500Name rootCaDN = CertsHelper.buildCertDN("CN", "jd", "jd", "rootCA");
+        X500Name middleCaDN = CertsHelper.buildCertDN("CN", "jd", "jd", "middleCA");
+        X500Name node0SigDN = CertsHelper.buildCertDN("CN", "jd", "jd", "node0-sig");
+        X500Name node0EncDN = CertsHelper.buildCertDN("CN", "jd", "jd", "node0-enc");
+
+        X509Certificate rootCACert = SmCertMarker.createRootCACert(rootKeyPair, rootCaDN, expire);
+        X509Certificate middleCACert = SmCertMarker.createMiddleCACert(rootKeyPair, rootCaDN, middleCaDN, middleKeyPair, expire);
+
+        X509Certificate node0SignCert = SmCertMarker.createEntitySignCert(middleKeyPair, middleCaDN, node0SigDN, node0SigKeyPair, expire);
+        X509Certificate node0EncCert = SmCertMarker.createEntityEncCert(middleKeyPair, middleCaDN, node0EncDN, node0EncKeyPair, expire);
+
+
+        JcaPEMWriter jcaPEMWriter = new JcaPEMWriter(new FileWriter("sig.cert.pem"));
+        jcaPEMWriter.writeObject(node0SignCert);
+        jcaPEMWriter.close();
+
+        jcaPEMWriter = new JcaPEMWriter(new FileWriter("sig.key.pem"));
+        jcaPEMWriter.writeObject(node0SigKeyPair.getPrivate());
+        jcaPEMWriter.close();
+
+        KeyStore signKeyStore = KeyStore.getInstance("pkcs12");
+        signKeyStore.load(null, null);
+        signKeyStore.setKeyEntry("sign", node0SigKeyPair.getPrivate(), "12345678".toCharArray(), new Certificate[]{middleCACert});
+        signKeyStore.store(new FileOutputStream("sign.pfx"), "12345678".toCharArray());
+
+        KeyStore encKeyStore = KeyStore.getInstance("pkcs12");
+        encKeyStore.load(null, null);
+        encKeyStore.setKeyEntry("enc", node0EncKeyPair.getPrivate(), "12345678".toCharArray(), new Certificate[]{middleCACert});
+        encKeyStore.store(new FileOutputStream("enc.pfx"), "12345678".toCharArray());
+
+
+        KeyStore bothStore = KeyStore.getInstance("pkcs12");
+        bothStore.load(null, null);
+
+        bothStore.setCertificateEntry("RCA", rootCACert);
+        bothStore.setCertificateEntry("OCA", middleCACert);
+        bothStore.setCertificateEntry("enc", node0EncCert);
+        bothStore.setCertificateEntry("sig", node0SignCert);
+        bothStore.setKeyEntry("enc", node0EncKeyPair.getPrivate(), "12345678".toCharArray(), new Certificate[]{node0EncCert, middleCACert, rootCACert});
+        bothStore.setKeyEntry("sig", node0SigKeyPair.getPrivate(), "12345678".toCharArray(), new Certificate[]{node0SignCert, middleCACert, rootCACert});
+
+
+        bothStore.store(new FileOutputStream("both.pfx"), "12345678".toCharArray());
+
+
+        System.out.println(".......");
+        System.out.println(rootCACert);
+        System.out.println(".......");
+        System.out.println(middleCACert);
+        System.out.println(".......");
+        System.out.println(node0EncCert);
+        System.out.println(".......");
+        System.out.println(node0SignCert);
+
+
+    }
+
+    @Test
+    public void testPemLoad() throws IOException, CertificateException {
 
         GmSSLProvider.enableGMSupport(GmSSLProvider.GMTLS);
 
-        String trustStoreFile = "all.truststore";
-        KeyStore trustStore = KeyStore.getInstance("pkcs12");
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        Certificate cert = cf.generateCertificate(new FileInputStream("root.ca.pem"));
+        System.out.println(cert);
+
+        cert = cf.generateCertificate(new FileInputStream("middle.ca.pem"));
+        System.out.println(cert);
+
+    }
+
+
+    @Test
+    public void test() throws Exception {
+
+        GmSSLProvider.enableGMSupport(GmSSLProvider.GMTLS);
+
+        String trustStoreFile = "client.jks";
+        KeyStore trustStore = KeyStore.getInstance("jks");
         trustStore.load(null, null);
 
 
-        String pfx = "D:\\block160\\cert\\sm2.127.0.0.1.both.pfx";
+        String pfx = "D:\\block160\\cert\\sm2.node0\\sm2.node0.enc.pfx";
         KeyStore keyStore = KeyStore.getInstance("pkcs12");
         keyStore.load(new FileInputStream(pfx), "12345678".toCharArray());
+
+        String sig = "D:\\block160\\cert\\sm2.node0\\sm2.node0.sig.pfx";
+        KeyStore keyStoresig = KeyStore.getInstance("pkcs12");
+        keyStoresig.load(new FileInputStream(sig), "12345678".toCharArray());
+
+
+        String aEnc = "enc.pfx";
+        KeyStore aEncKS = KeyStore.getInstance("pkcs12");
+        aEncKS.load(new FileInputStream(aEnc), "12345678".toCharArray());
+
+        String aSign = "sign.pfx";
+        KeyStore aSignKS = KeyStore.getInstance("pkcs12");
+        aSignKS.load(new FileInputStream(aSign), "12345678".toCharArray());
+
+        String both = "D:\\block160\\cert\\sm2.node0\\sm2.node0.both.pfx";
+        KeyStore keyStoreboth = KeyStore.getInstance("pkcs12");
+        keyStoreboth.load(new FileInputStream(both), "12345678".toCharArray());
+
+        String botha = "both.pfx";
+        KeyStore bothStore = KeyStore.getInstance("pkcs12");
+        bothStore.load(new FileInputStream(botha), "12345678".toCharArray());
+
+        System.out.println("-------------");
+
+//        System.out.println(keyStoreboth.getCertificate("rca"));
+//        System.out.println(keyStoreboth.getCertificate("oca"));
+//        System.out.println(keyStoreboth.getCertificate("enc"));
+//        System.out.println(keyStoreboth.getCertificate("sig"));
+
+
+        // keyboth:  Sign key, Enc key
+        //           RCA certs  OCA certs sig certs  enc certs
+
+//        Signature signature = Signature.getInstance("SM3WithSM2");
+//
+//        PemReader pemReader = new PemReader(Files.newBufferedReader(new File("D:\\block160\\cert\\sm2.node0\\sm2.oca.pem").toPath()));
+//        PemObject pemObject = pemReader.readPemObject();
+//
+//        X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(pemObject.getContent());
+//        KeyFactory keyFactory = KeyFactory.getInstance("EC");
+//
+//        PublicKey publicKey = keyFactory.generatePublic(pubKeySpec);
+
+        keyStore = bothStore;
 
         Enumeration<String> aliases = keyStore.aliases();
 
@@ -72,19 +223,6 @@ public class TestKeyStore {
         trustStore.store(new FileOutputStream(trustStoreFile), "12345678".toCharArray());
 
 
-
-
     }
-
-    @Test
-    public void testCert(){
-
-        SM2P256V1Curve p256V1Curve = new SM2P256V1Curve();
-
-
-
-    }
-
-
 
 }
